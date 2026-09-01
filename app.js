d088db8b1aa94c9a739510f25adeabcc7586d301
Martin21-08 <espinoza.alejandro2108@gmail.com>
2026-09-01 12:24:06 -0400

    CookIQ

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1> INDEX </h1>');
+});
+
+app.get('/servicios', (req, res) => {
+    res.send('<h1> SERVICIOS </h1>');
+});
+
+app.get('/contacto', (req, res) => {
+    res.send('<h1> CONTACTO </h1>');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
+
+
